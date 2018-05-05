93d451fd035881b164bc67ff74d749187e19299a
ndurvasula <140.naveen.d@gmail.com>
2018-05-05 16:42:14 -0400

    Finish AI base class

--- env.py
+++ env.py
@@ -2,10 +2,11 @@ import numpy as np
 import GPy
 
 from scipy.optimize import minimize
+from scipy.stats import norm
 
 class AI():
 
-    def __init__(self, kv = 1, lv = 1, kl = 1, b = -1, mu, sigma, D):
+    def __init__(self, kv = 1, lv = 1, kl = 1, b = -1, mu, sigma, D, N, price, o=False):
         #kv = kernel variance, lv = likelihood variance, kl = kernel lengthscale
         #b is bin size (max number of points the AI considers)
 
@@ -24,6 +25,9 @@ class AI():
         self.sigma = sigma
 
         self.D = D
+        
+        self.N = n
+        self.price = price
 
     def update(self, rphi, theta):
 
@@ -40,6 +44,9 @@ class AI():
         self.GP.kern.lengthscale = kl
         self.GP.likelihood.variance = lv
 
+        if o:
+            self.GP.optimize()
+
     def opt(rphi):
 
         theta = self.GP.predict(np.array([rphi]))[0][0]
@@ -53,5 +60,56 @@ class AI():
     def act():
 
         rphi = mimimize(opt, 0, method='BFGS', jac=dopt).x
+        phi = []
+        theta = self.GP.predict(np.array([rphi]))[0][0]
+        psi = rphi/(self.D*theta + np.random.normal(self.mu, self.sigma))
+        
+        if psi > 1:
+            psi = 1
+            
+        if psi < 0:
+            psi = 0
+
+        psi /= 2
+        
+        K = .3
+        MEANS = np.array([i*1.0/(self.N-1) for i in range(self.N)])
+        STD = 1/(6*(self.N-1))
+        
+        pmf = np.array([norm.cdf(psi+.5+K, loc=MEANS[i], scale=STD) - norm.cdf(psi+.5-K, loc=MEANS[i], scale=STD) for i in range(self.N)])
+        pmf /= np.sum(pmf)
+
+        price = 0
+
+        while rphi > 0:
+            product = list(np.random.multinomial(1, pmf)).index(1)
+            rphi -= self.price(product)
+            price += self.price(product)
+            phi.append([product])
+
+        return np.array(phi), price
+
+#Arbitary as of now
+def price(product):
+    return product**2
+
+TIME = 0
+NATIONS = 3
+N = 3
+D = [5000000000,1000000000,2000000000,3000000000] #untariffed profit in N_0
+
+#Mu_i is the sum of the profits from all nations (not N_0) - sum of other nations' profits in N_i
+MU = [2000000000,1000000000,4500000000]
+STD = [5000000,6000000,3000000]
+
+KV = [1,.5,5]
+
+
+            
+            
+            
+        
+        
+        
         
     
