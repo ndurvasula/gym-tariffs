8f68ec206644a2eb4e9e24e3c98b4296996bb17c
ndurvasula <140.naveen.d@gmail.com>
2018-05-09 22:05:57 -0400

    Fix bugs in environment

--- gym_tariffs/envs/tariff_env.py
+++ gym_tariffs/envs/tariff_env.py
@@ -1,6 +1,9 @@
 import numpy as np
 import GPy
 
+import warnings
+warnings.filterwarnings("ignore")
+
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
@@ -17,11 +20,13 @@ class AI():
 
         self.GP = None
 
-        self.X = np.array([])
-        self.Y = np.array([])
+        #Prior function is a line
+        self.X = np.array([[0],[1]])
+        self.Y = np.array([[0],[1]])
         
         self.kv = kv
         self.kl = kl
+        self.o=o
 
         self.b = b
 
@@ -32,42 +37,46 @@ class AI():
 
         self.K = K
         
-        self.N = n
+        self.N = N
         self.price = price
 
-        self.action = None
+        self.action = False
 
     def update(self, theta):
 
-        if b > 0 and len(self.X) == b:
+        if self.b > 0 and len(self.X) == self.b:
             del self.X[0]
             del self.Y[0]
             
-        self.X = np.append(self.X, [self.action], axis=0)
-        self.Y = np.append(self.Y, [theta], axis=0)
+        if self.action:
+            self.X = np.append(self.X, [self.action/(self.D+self.mu)], axis=0)
+            self.Y = np.append(self.Y, [theta], axis=0)
 
         self.GP = GPy.models.GPRegression(self.X, self.Y)
-        
-        self.GP.kern.variance = kv
-        self.GP.kern.lengthscale = kl
 
-        if o:
+        if self.o:
             self.GP.optimize()
 
-    def opt(rphi):
+        else:
+            self.GP.kern.variance = self.kv
+            self.GP.kern.lengthscale = self.kl
 
-        theta = self.GP.predict(np.array([rphi]))[0][0]
+    def opt(self,rphi):
+
+        theta = self.GP.predict(np.array([rphi/(self.D+self.mu)]))[0][0]
+        #print("F:",(-rphi + self.mu + self.D*(self.D+self.mu)*theta))
         return -1* (-rphi + self.mu + self.D*theta)
 
-    def dopt(rphi):
+    def dopt(self,rphi):
 
-        dtheta = self.GP.predictive_gradients(np.array([rphi]))[0][0][0]
-        return -1* (-1 + self.D*dtheta)
+        dtheta = self.GP.predictive_gradients(np.array([rphi/(self.D+self.mu)]))[0][0][0]
+        #print("dF:",(-1 + self.D*(self.D+self.mu)*dtheta))
+        return -1* (-1 + self.D*1/(self.D+self.mu)*dtheta)
 
     def act(self):
 
-        rphi = mimimize(opt, 0, method='BFGS', jac=dopt).x
-        self.action = rphi
+        rphi = minimize(self.opt, np.array([1]), bounds = ((0,self.D+self.mu),), jac=self.dopt, tol=1e-10).x
+        print(rphi, -1*self.opt(rphi))
         
         theta = self.GP.predict(np.array([rphi]))[0][0]
         psi = rphi/(self.D*theta + np.random.normal(self.mu, self.sigma))
@@ -79,13 +88,14 @@ class AI():
             psi = 0
 
         psi /= 2
-        psi = (.5-psi)
+        psi = (1-psi)
         
         
         MEANS = np.array([i*1.0/(self.N-1) for i in range(self.N)])
         STD = 1/(6*(self.N-1))
         
-        pmf = np.array([norm.cdf(psi+.5+self.K, loc=MEANS[i], scale=STD) - norm.cdf(psi+.5-self.K, loc=MEANS[i], scale=STD) for i in range(self.N)])
+        pmf = np.array([norm.cdf(psi+self.K, loc=MEANS[i], scale=STD) - norm.cdf(psi-self.K, loc=MEANS[i], scale=STD) for i in range(self.N)])
+        pmf.shape = self.N
         pmf /= np.sum(pmf)
 
         price = 0
@@ -98,6 +108,8 @@ class AI():
             price += self.price(product)
             phi[product] += 1
 
+        self.action = np.array([price])
+
         return phi, price
 
 #Arbitary as of now
@@ -109,7 +121,7 @@ NATIONS = 3
 DAYS = 365
 N = 3
 PRODUCT_BASE = 100000
-D = [5000000000,1000000000,2000000000,3000000000] #untariffed profit in N_0
+D = [5000000000,10000000000,20000000000,30000000000] #untariffed profit in N_0
 
 #Mu_i is the sum of the profits from all nations (not N_0) - sum of other nations' profits in N_i
 MU = [2000000000,1000000000,4500000000]
@@ -118,8 +130,10 @@ STD = [5000000,6000000,3000000]
 KV = [1,.5,2]
 KL = [1,.5,2]
 B = [10,20,-1]
-O = [False, False, True]
+O = [True, True, True]
 
+DISCRETIZE = False
+DELTA = 100
 
 class TariffEnv(gym.Env):
     metadata = {'render.modes' : ['human']}
@@ -148,14 +162,16 @@ class TariffEnv(gym.Env):
             action = action*1.0/DELTA
 
         for i in range(NATIONS):
-            self.AI[i].update(action[i])
+            self.AI[i].update(np.array([action[i]]))
 
-        res = np.array([self.AI.act() for i in range(NATIONS)])
+        res = np.array([self.AI[i].act() for i in range(NATIONS)])
+        print(res)
+        
         states, rewards = zip(*res)
         obs = np.sum(np.array(states),axis=0)
         reward = np.sum(np.array(rewards)) + D[0]*(NATIONS+1-np.sum(action))
 
-        obs, reward, self.time == DAYS-1, {}
+        return obs, reward, self.time == DAYS-1, {}
 
     def render(self, mode='human', close='False'):
         pass
